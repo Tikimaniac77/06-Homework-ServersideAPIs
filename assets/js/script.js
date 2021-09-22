a2bbb2b922a8c750ae18d541edc3ecdeeecbab90
Tiki <michaelwachtel@yahoo.com>
2021-09-22 00:47:10 -0700

    updated JS to generate 5 day forecast dynamically. updated CSS to adjust 5 day elements. updated HTML to bootstrap format for 5 day.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,20 @@ var submitBtn = document.getElementById('submitBtn');
 
 var listBtn = document.querySelectorAll('.listBtn');
 
-var APIKey = "f7c5269b7ff70849ebb5f47fde1d3606";
+var forecast = document.getElementById('forecast');
+
+var APIKey = "36d5883fc0309cad41a9117e54bcfc78";
 
 var listContainer = [];
 
 
 //function will take city name input into form and generate elements dynamically on page using weatherAPI fetch requests.
 function getApi(event) {
+    currentWeather.innerHTML = '';
+            
     var cityInput = document.getElementById('cityInput').value;
-    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&APPID=" + APIKey;
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&APPID=" + APIKey;
+    
     console.log(cityInput);
     console.log(requestUrl);
     event.preventDefault();
@@ -44,13 +49,13 @@ function getApi(event) {
           
           cityName.innerHTML = data.name + " (" +today+")";
           
-          var icon = data.weather[0].icon;
-          //var iconURL = "https://openweathermap.org/img/wn/" + icon + "2x.png";
+          // to use API to generate weather icons
+          var icon = data.weather[0].icon;          
           var iconImg = document.createElement('img');
           iconImg.src = "https://openweathermap.org/img/wn/" + icon + ".png";
 
 
-
+          //Generates rest of elements on input city with fetch from API
           var cityTemp = document.createElement('p');
           cityTemp.innerHTML = "<strong>Temperature:</strong> " + data.main.temp + " \xB0f";
 
@@ -59,7 +64,8 @@ function getApi(event) {
 
           var cityHumidity = document.createElement('p');
           cityHumidity.innerHTML = "<strong>Humidity:</strong> " + data.main.humidity + " %";          
-
+          
+          //To fetch UV index specifically needed to use other API call and reference data from previous api call.
           var cityUVI = document.createElement('p');
 
           var cityLat = data.coord.lat;
@@ -76,17 +82,122 @@ function getApi(event) {
                     console.log(data);
                     
                     cityUVI.innerHTML = "<strong>UV Index:</strong> " + data.current.uvi;
+                    //logic to change UV index color class
+                    if(data.current.uvi < 2) {
+                        cityUVI.classList.add('uvLow');
+                    }else if(data.current.uvi > 6){
+                        cityUVI.classList.remove('uvMod');                        
+                        cityUVI.classList.add('uvHigh');
+                    }else{
+                        cityUVI.classList.remove('uvLow');
+                        cityUVI.classList.add('uvMod');
+                    }
                 })
           }
 
-          getUVapi();
+          getUVapi();        
           
-          
-
           resultBody.append(cityName, iconImg, cityTemp, cityWind, cityHumidity, cityUVI);
 
           currentWeather.append(resultDiv);
 
+          var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&units=imperial&appid=" + APIKey;
+          
+          //fetch and generate elements for 5 day forecast
+          fetch(forecastURL)
+            .then(function (res){
+                console.log(res.status);
+                return res.json();
+            })
+            .then(function(dat){
+                console.log(dat);
+                                
+                var daysArray = [];
+
+                let d1 = {
+                    date: moment(dat.list[0].dt_txt, "YYYY-MM-DD").format("L"),
+                    icon: dat.list[0].weather[0].icon,
+                    temp: dat.list[0].main.temp,
+                    wind: dat.list[0].wind.speed,
+                    humid: dat.list[0].main.humidity                    
+                };
+                daysArray.push(d1);
+                
+                let d2 = {
+                    date: moment(dat.list[8].dt_txt, "YYYY-MM-DD").format("L"),
+                    icon: dat.list[8].weather[0].icon,
+                    temp: dat.list[8].main.temp,
+                    wind: dat.list[8].wind.speed,
+                    humid: dat.list[8].main.humidity                  
+                };
+                daysArray.push(d2);
+
+                let d3 = {
+                    date: moment(dat.list[16].dt_txt, "YYYY-MM-DD").format("L"),
+                    icon: dat.list[16].weather[0].icon,
+                    temp: dat.list[16].main.temp,
+                    wind: dat.list[16].wind.speed,
+                    humid: dat.list[16].main.humidity                  
+                };
+                daysArray.push(d3);
+
+                let d4 = {
+                    date: moment(dat.list[24].dt_txt, "YYYY-MM-DD").format("L"),
+                    icon: dat.list[24].weather[0].icon,
+                    temp: dat.list[24].main.temp,
+                    wind: dat.list[24].wind.speed,
+                    humid: dat.list[24].main.humidity                  
+                };
+                daysArray.push(d4);
+
+                let d5 = {
+                    date: moment(dat.list[32].dt_txt, "YYYY-MM-DD").format("L"),
+                    icon: dat.list[32].weather[0].icon,
+                    temp: dat.list[32].main.temp,
+                    wind: dat.list[32].wind.speed,
+                    humid: dat.list[32].main.humidity                  
+                };
+                daysArray.push(d5);                
+                console.log(daysArray);
+
+                //creates parent div
+                var resultDiv2 = document.createElement('div');
+                resultDiv2.classList.add('row', 'card', 'bg-light', 'text-dark', 'p-3');
+                resultDiv2.setAttribute('id', "forecastDiv");
+                                                          
+                    for (var i = 0; i < daysArray.length; i++) { 
+                        
+                    forecast.innerHTML = '';
+                                                         
+                    //creates body div
+                    var resultBody2 = document.createElement('div');
+                    resultBody2.setAttribute('id',"day" + i);
+                    resultBody2.classList.add('col-auto', 'card-body');
+                    resultDiv2.append(resultBody2);                                                       
+                                       
+                    var forecastDates = document.createElement('h4');
+                    forecastDates.innerHTML = daysArray[i].date;
+                    
+                    var icon = daysArray[i].icon;          
+                    var iconImg2 = document.createElement('img');
+                    iconImg2.src = "https://openweathermap.org/img/wn/" + icon + ".png";
+
+                    var forecastTemp = document.createElement('p');
+                    forecastTemp.innerHTML = "Temp: " + daysArray[i].temp + " \xB0f";
+                    
+
+                    var forecastWind = document.createElement('p');
+                    forecastWind.innerHTML = "Wind: " + daysArray[i].wind + " mph";
+                    
+
+                    var forecastHumid = document.createElement('p');
+                    forecastHumid.innerHTML = "Humidity: " + daysArray[i].humid + " %";
+                    
+                    resultBody2.append(forecastDates, iconImg2, forecastTemp, forecastWind, forecastHumid);
+                    forecast.append(resultDiv2);                    
+                }                                
+            
+            })
                    
           var listText = document.getElementById('cityInput').value.trim();
 
@@ -115,10 +226,18 @@ function renderList() {
       cityListItems.classList.add('listBtn')
       cityListItems.textContent = listItem;
 
+      //trying to get cityList items to button click
+      cityListItems.addEventListener('click', function(event){
+       cityInput = "";
+       cityInput = event.target.textContent;
+       console.log(cityInput);
+      })
+
       cityList.append(cityListItems);
     }
   }
-    function init() {
+
+function init() {
         var storeList = JSON.parse(localStorage.getItem('cities'));
 
         if (storeList !== null) {
@@ -128,14 +247,16 @@ function renderList() {
         renderList();
     }
     
-    function storedList(){
+function storedList(){
         localStorage.setItem('cities', JSON.stringify(listContainer));
     }
 
 
 
+
 submitBtn.addEventListener('click', getApi);
 
-//listBtn.addEventListener('click', function);
+
+//listBtn.addEventListener('click', getApi);
 
 init();
